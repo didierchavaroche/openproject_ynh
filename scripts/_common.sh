b5acfb0d84947e1b3c0a371395760c2937109e04
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-05-26 09:25:08 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,8 +7,8 @@
 PROJECT_VERSION=12
 
 # dependencies used by the app
-pkg_dependencies="apt-transport-https memcached postgresql postgresql-common"
-extra_pkg_dependencies="openproject"
+#REMOVEME? pkg_dependencies="apt-transport-https memcached postgresql postgresql-common"
+#REMOVEME? extra_pkg_dependencies="openproject"
 
 #=================================================
 # PERSONAL HELPERS
